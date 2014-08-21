9a2dfecee8d31b1314c62d6fdf3c0861d1d8e575
Staphany Park <stapark008@gmail.com>
2014-08-21 08:01:51 -0400

    Favicon

--- bin/load_venues.rb
+++ bin/load_venues.rb
@@ -18,13 +18,20 @@ businesses.each do |business|
   page = agent.get new_venue_url
   form = page.form(action: /\/venues$/)
   form.field(name: /name/).value = business['name']
-  form.field(name: /city_id/).option(text: business['city']).select
+  city_option = form.field(name: /city_id/).option(text: business['city'])
+  if city_option 
+    city_option.select
+  else
+    p business['city']
+    p business
+    next
+  end
   form.field(name: /lat/).value = business['location']['lat']
   form.field(name: /long/).value = business['location']['long']
   form.field(name: /image_url/).value = business['image_url']
   form.field(name: /stars/).value = business['rating']['stars']
   form.field(name: /rating/).value = business['rating']['count']
   form.field(name: /yelp_id/).value = business['yelp_id']
-  
+    
   form.click_button
 end
