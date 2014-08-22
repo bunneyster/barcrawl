5d3e3e4832946562a028180bb455d063d011d5ab
Staphany Park <stapark008@gmail.com>
2014-08-22 17:53:14 -0400

    Revised Venue model test to reflect schema changes.

--- test/models/venue_test.rb
+++ test/models/venue_test.rb
@@ -5,9 +5,12 @@ class VenueTest < ActiveSupport::TestCase
   setup do
     @venue = Venue.new name: 'A Valid Venue',
                        city: cities(:paris),
-                       cid: 1234567890,
                        latitude: cities(:paris).latitude,
-                       longitude: cities(:paris).longitude
+                       longitude: cities(:paris).longitude,
+                       stars: 5.0,
+                       rating_count: 1000,
+                       image_url: 'http://venue.jpg',
+                       yelp_id: 'a-valid-venue'
   end
   
   test "setup creates valid venue" do
@@ -28,13 +31,6 @@ class VenueTest < ActiveSupport::TestCase
     assert_match(/can't be blank/, @venue.errors[:city].inspect)
   end
   
-  test "venue must have cid" do
-    @venue.cid = nil
-    
-    assert @venue.invalid?
-    assert_match(/can't be blank/, @venue.errors[:cid].inspect)
-  end
-  
   test "venue must have latitude" do
     @venue.latitude = nil
     
@@ -49,18 +45,39 @@ class VenueTest < ActiveSupport::TestCase
     assert_match(/can't be blank/, @venue.errors[:longitude].inspect)
   end
   
-  test "venue name must be unique" do
-    venue = Venue.new name: venues(:cafe).name
+  test "venue must have a star rating" do
+    @venue.stars = nil
     
-    assert venue.invalid?
-    assert_match(/has already been taken/, venue.errors[:name].inspect)
+    assert @venue.invalid?
+    assert_match(/can't be blank/, @venue.errors[:stars].inspect)
   end
   
-  test "venue cid must be unique" do
-    venue = Venue.new cid: venues(:cafe).cid
+  test "venue star rating must be a multiple of 0.5 between 1 and 5" do
+    @venue.stars = 0
+    
+    assert @venue.invalid?
+    assert_match(/is not included in the list/, @venue.errors[:stars].inspect)
+  end
+  
+  test "venue must have a rating count" do
+    @venue.rating_count = nil
+    
+    assert @venue.invalid?
+    assert_match(/can't be blank/, @venue.errors[:rating_count].inspect)
+  end
+  
+  test "venue must have a yelp id" do
+    @venue.yelp_id = nil
+    
+    assert @venue.invalid?
+    assert_match(/can't be blank/, @venue.errors[:yelp_id].inspect)
+  end
+  
+  test "venue yelp id must be unique" do
+    venue = Venue.new yelp_id: venues(:cafe).yelp_id
     
     assert venue.invalid?
-    assert_match(/has already been taken/, venue.errors[:cid].inspect)
+    assert_match(/has already been taken/, venue.errors[:yelp_id].inspect)
   end
   
   test "venue can't be destroyed if referenced by tour stops" do
