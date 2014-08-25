834a5015db1fa6bd21e89b03987de637edf36648
Staphany Park <stapark008@gmail.com>
2014-08-25 09:42:19 -0400

    Updated SessionsController tests. Moved HomeController tests into SessionsController tests.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,16 +6,31 @@ class SessionsControllerTest < ActionController::TestCase
   setup do
     @user = users(:peridot)
   end
+   
+  test "logged user renders dashboard" do
+    user = users(:peridot)
+    session[:user_id] = user.to_param
+    get :new
+    assert_response :success
+    assert_template :dashboard
+  end
+  
+  test "unlogged user renders welcome" do
+    session[:user_id] = nil
+    get :new
+    assert_response :success
+    assert_template :welcome
+  end
 
   test "valid login redirects to root" do
-    post :create, email: @user.email, password: 'peridot'
+    post :create, session: { email: @user.email, password: 'peridot' }
     assert_match(/Welcome/, flash[:alert])
     assert_redirected_to root_url
-    assert_equal users(:peridot).id, session[:user_id]
+    assert_equal users(:peridot).to_param, session[:user_id]
   end
   
   test "invalid login redirects to root" do
-    post :create, email: @user.email, password: 'not peridot'
+    post :create, session: { email: @user.email, password: 'not peridot' }
     assert_match(/Invalid/, flash[:alert])
     assert_redirected_to root_url
   end
